4319825da4ceb31737871ec31dde3822bbecb211
Yuxi <liuyuxi.yx@gmail.com>
2021-08-05 12:59:43 +0800

    Added exercises folder

--- 30daysofreact/src/components/BlockList.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const BlockList = ({ list, style }) => {
-    return list.map(item => <div className='block' style={style} key={item}>{ item }</div>)
-}
-
-export default BlockList
